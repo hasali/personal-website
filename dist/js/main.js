d091bb239b7e85411beb664aabd9a75a83371ff6
hassan <hasalimah@gmail.com>
2021-04-12 13:16:00 -0400

    big push cuz of fuck up

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,46 +5,24 @@ const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.nav-bar');
 
 const lidWrapper = document.querySelectorAll(".lid-wrapper");
-const lid = document.querySelector(".lid");
+const lid = document.querySelectorAll(".lid");
 const sleeveItem = document.querySelectorAll(".sleeve-item");
 
-let showMenu = true;
-let showSleeveItem = true;
+
  
-menuBtn.addEventListener('click', toggleMenu());
+menuBtn.addEventListener('click', ()=>{
+    console.log('hello from menubtn');
+    hamburger.classList.toggle('open');
+    nav.classList.toggle('open');
+    menuNav.classList.toggle('open');
+});
 
 
 for(var i = 0; i < lidWrapper.length; i++){
     (function(i){
-        lidWrapper[i].addEventListener('click', function(){       
-            document.getElementsByClassName('sleeve-item')[i].classList.toggle("slide");
-        });
-    })(i);
-    
-    
-}
-function toggleLid(){
-    
-    
-    for (var i=0; i<sleeveItem.length;i++){
-        
+        lidWrapper[i].addEventListener('click', function(){  
+            lid[i].classList.toggle("slide");     
             sleeveItem[i].classList.toggle("slide");
-        
-        console.log('hello');
-            
-    }
-}
-function toggleMenu(){
-    
-        hamburger.classList.toggle('open');
-        nav.classList.toggle('open');
-        menuNav.classList.toggle('open');
-        //showMenu = true;
-    
-    // }else{
-    //     hamburger.classList.remove('open');
-    //     nav.classList.remove('open');
-    //     menuNav.classList.remove('open');
-    //     showMenu = false;
-    // }
+        });
+    })(i);    
 }
