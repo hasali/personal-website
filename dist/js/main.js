969ddcf78067c74fb9fe52d94904ef2cde35531b
Hassan <hasalimah@gmail.com>
2022-02-18 23:38:53 -0500

    Menu button appears only after landing page + Menu closes appropriately + Landing Page nav recolor

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,24 +7,49 @@ const navItems = document.querySelectorAll('.nav-item');
 const lidWrapper = document.querySelectorAll(".lid-wrapper");
 const lid = document.querySelectorAll(".lid");
 const sleeveItem = document.querySelectorAll(".sleeve-item");
+const landingPage = document.querySelector(".landing-page-wrapper");
+var isMenuOpen = false;
 
 
- 
-menuBtn.addEventListener('click', ()=>{
+menuBtn.style.display = "none";
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > landingPage.scrollHeight) {
+        menuBtn.style.display = "block"
+    } else {
+        if(isMenuOpen){
+            isMenuOpen =false;
+            menuEvent();
+        }
+        menuBtn.style.display = "none";
+    }
+    console.log(isMenuOpen);
+})
+
+
+
+
+
+
+menuBtn.addEventListener('click', () => {
     console.log('hello from menubtn');
-    hamburger.classList.toggle('open');
-    nav.classList.toggle('open');
-    menuNav.classList.toggle('open');
-    navItems.forEach(item => item.classList.toggle('open'));     
+    isMenuOpen = !isMenuOpen;
+    menuEvent();
     
 });
 
+function menuEvent(){
+    hamburger.classList.toggle('open');
+    nav.classList.toggle('open');
+    menuNav.classList.toggle('open');
+    navItems.forEach(item => item.classList.toggle('open'));
+};
 
-for(var i = 0; i < lidWrapper.length; i++){
-    (function(i){
-        lidWrapper[i].addEventListener('click', function(){  
-            lid[i].classList.toggle("slide");     
+for (var i = 0; i < lidWrapper.length; i++) {
+    (function (i) {
+        lidWrapper[i].addEventListener('click', function () {
+            lid[i].classList.toggle("slide");
             sleeveItem[i].classList.toggle("slide");
         });
-    })(i);    
+    })(i);
 }
